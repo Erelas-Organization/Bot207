999e49ad05cf943b41c20142926a9814ac3f6db4
Erela <erelaa@hotmail.com>
2023-11-18 00:45:37 +0100

    stfu jesus event

---
+++ src/events/stfujesus.ts
@@ -0,0 +1,15 @@
+import { Client, Message } from "discord.js";
+import { BotEvent } from "../types";
+
+const stfuEvent: BotEvent = {
+  name: "messageCreate",
+  execute: async (message: Message) => {
+    if (!message.author.bot) {
+      if (message.author.id === "440849314487468032") {
+        message.channel.send("stfu " + "<@" + message.author.id + ">");
+      }
+    }
+  },
+};
+
+export default stfuEvent;
