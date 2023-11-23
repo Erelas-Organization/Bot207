0b6475a71f0f101651b3824a2eebbbadbf7fd029
Tim Chen <Tim.chen@maibornwolff.de>
2023-11-23 16:23:39 +0100

    add basic play music functionality with discord-player

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { Client, GatewayIntentBits, Collection, ActivityType } from "discord.js";
-const { Guilds, MessageContent, GuildMessages, GuildMembers } = GatewayIntentBits;
-const client = new Client({ intents: [Guilds, MessageContent, GuildMessages, GuildMembers] });
+import { Client, GatewayIntentBits, Collection, ActivityType, EmbedBuilder } from "discord.js";
+import { Player } from "discord-player";
+const { Guilds, MessageContent, GuildMessages, GuildMembers, GuildVoiceStates } = GatewayIntentBits;
+const client = new Client({ intents: [Guilds, MessageContent, GuildMessages, GuildMembers, GuildVoiceStates] });
+const player = new Player(client);
 import { Command, SlashCommand } from "./types";
 import { config } from "dotenv";
 import { promises as fsPromises } from "fs";
@@ -23,6 +25,19 @@ async function loadHandlers() {
         }
     }));
 }
+
+async function loadPlayerExtractors() {
+    await player.extractors.loadDefault();   
+}
+
+loadPlayerExtractors().then(()=>{
+    console.log("Player Extractors loaded")
+})
+
+player.events.on('playerStart', (queue, track) => {
+    const embed = new EmbedBuilder().setTitle("Now playing - " + track.title).setDescription(track.author).addFields({name: "\u200b", value: "`"+ track.duration +"`"}).setThumbnail(track.thumbnail)
+    queue.metadata.channel.send({embeds: [embed]});
+});
 
 client.on('ready', () => {
     if (client.user) {
---
+++ src/slashCommands/play.ts
@@ -0,0 +1,42 @@
+import { SlashCommandBuilder } from "discord.js"
+import { SlashCommand } from "../types";
+import { useMainPlayer } from "discord-player";
+
+const command: SlashCommand = {
+  command: new SlashCommandBuilder()
+    .setName("play")
+    .addStringOption(option => {
+      return option
+        .setName("query")
+        .setDescription("Query or URL of the requested track")
+        .setRequired(true);
+    })
+    .setDescription("Play the requested track"),
+  execute: async (interaction) => {
+    const player = useMainPlayer();
+    const guild = interaction.guild;
+    const member = guild?.members.cache.get(interaction.user.id)
+    const channel = member?.voice.channel
+    if (!channel) return interaction.reply('You are not connected to a voice channel!');
+    const query = interaction.options.getString('query', true);
+
+    await interaction.deferReply();
+
+    try {
+        const { track } = await player.play(channel, query, {
+            nodeOptions: {
+                metadata: interaction
+            }
+        });
+
+        return interaction.followUp(`**${track.title}** enqueued!`);
+    } catch (e) {
+        // let's return error if something failed
+        return interaction.followUp(`Something went wrong: ${e}`);
+    }
+
+  },
+  cooldown: 10
+}
+
+export default command
