7f0184a3ad9e0a717b2976f40e3fa4b5d104d09e
Clodolt <63547957+Clodolt@users.noreply.github.com>
2023-11-24 02:27:28 +0100

    update play.ts for invisible message

--- src/slashCommands/play.ts
+++ src/slashCommands/play.ts
@@ -13,40 +13,37 @@ const command: SlashCommand = {
     })
     .setDescription("Play the requested track"),
   execute: async (interaction) => {
-
-
     const player = useMainPlayer();
     const guild = interaction.guild;
-    const member = guild?.members.cache.get(interaction.user.id)
-    const channel = member?.voice.channel
-    if (!channel) return interaction.reply({content: 'You are not connected to a voice channel!', ephemeral: true});
+    const member = guild?.members.cache.get(interaction.user.id);
+    const channel = member?.voice.channel;
+    if (!channel) return interaction.reply({ content: 'You are not connected to a voice channel!', ephemeral: true });
     const query = interaction.options.getString('query', true);
 
-    if(!guild) return interaction.reply({content: 'This command can only be used on a server', ephemeral: true})
+    if (!guild) return interaction.reply({ content: 'This command can only be used on a server', ephemeral: true });
+
+    if (player.queues.get(guild.id) && player.queues.get(guild.id)?.isPlaying) {
+      const { track } = await player.play(channel, query);
+      return interaction.reply({ content: track.title + " added to queue!", ephemeral: true });
+    }
 
     await interaction.deferReply();
 
     try {
-      if (player.queues.get(guild.id) && player.queues.get(guild.id)?.isPlaying) {
-        const { track } = await player.play(channel, query);
-        return interaction.followUp({content: track.title + " added to queue!", ephemeral: true});
-        } else {
-          const { track } = await player.play(channel, query, {
-            nodeOptions: {
-                volume: 10,
-                metadata: interaction
-            }
-        });
-        const embed = new EmbedBuilder()
-        .setTitle("Loading " + track.title)
-        return interaction.followUp({embeds: [embed]});
-      }
+      const { track } = await player.play(channel, query, {
+        nodeOptions: {
+          volume: 10,
+          metadata: interaction
+        }
+      });
+      const embed = new EmbedBuilder()
+        .setTitle("Loading " + track.title);
+      return interaction.followUp({ embeds: [embed] });
     } catch (error) {
-        return interaction.followUp(`Something went wrong: ${error}`);
+      return interaction.followUp(`Something went wrong: ${error}`);
     }
-
   },
   cooldown: 10
 }
 
-export default command
+export default command;
