d8a35982b891b754a30d1daeda7104e86676a7d2
Erelaa <erelaa@hotmail.com>
2023-12-03 13:03:06 +0100

    add mock and birb command

--- src/events/stfujesus.ts
+++ src/events/stfujesus.ts
@@ -5,8 +5,17 @@ const stfuEvent: BotEvent = {
   name: "messageCreate",
   execute: async (message: Message) => {
     if (!message.author.bot && message.author.id === "440849314487468032") {
-        message.channel.send("stfu " + "<@" + message.author.id + ">");
+      try {
+        const response = await fetch(`https://api.popcat.xyz/mock?text=${encodeURIComponent(message.content)}`);
+        const data = await response.json();
+        if (data.text) {
+          await message.channel.send(`${data.text} - *in a mocking tone*`);
+          await message.channel.send("stfu " + "<@" + message.author.id + ">");
+        } 
+      } catch (error) {
+          console.error('Error mocking text:', error);
       }
+    }
   },
 };
 
